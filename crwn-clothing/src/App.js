4477c2a0c2c544586168cf3d9e915faabbfcff46
jaewonYoon <21300492@handong.edu>
2019-10-25 20:36:19 +0900

    whatever

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import HomePage from './pages/homepage/homepage.component';
+const App = () => {
+  return(
+    <div>
+      <HomePage/>
+    </div>
+  )
+}
+
+export default App;
