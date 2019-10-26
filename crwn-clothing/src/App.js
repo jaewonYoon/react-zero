a6214fe149bf24f7c2f1229355c6becdab41ff98
jaewonYoon <21300492@handong.edu>
2019-10-26 13:39:29 +0900

    update our menu-items to navigate withRouter HOC

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import { Link, Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+const TopicsList = (props) => (
+  <div>
+    <h1>TopicsList</h1>
+  </div>
+);
+const TopicDetail = (props) => {
+  console.log(props);
+  return(
+  <div>
+    <h1>Topic Detail Page</h1>
+  </div>);
+}
 const App = () => {
   return(
     <div>
-      <HomePage/>
+        <Route exact path="/" component = {HomePage} />
+        <Route exact path='/topics' component = {TopicsList}/>
+        <Route path='/topics/:topicId' component ={TopicDetail} />
     </div>
   )
 }
