ba2194c204b2f16207904168755837240d68d5f8
jaewonYoon <21300492@handong.edu>
2019-11-02 23:46:33 +0900

    README edit

---
+++ crwn-clothing/src/redux/cart/cart.selector.js
@@ -0,0 +1,14 @@
+import {createSelector} from 'reselect';
+
+const selectCart = state => state.cart;
+
+const selectUser = state => state.user
+
+export const selectCartItems = createSelector(
+    [selectCart],
+    (cart) => cart.cartItems
+)
+
+export const selectCartItemsCount = createSelector ( 
+    [selectCartItems ]
+)
